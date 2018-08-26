4aaa9f9f13fff0a86210f3fab65f9fa8620c3e51
tintin.young <tintinr@126.com>
2018-08-26 15:53:57 +0800

    refactor makefile

--- test_inline.mk
+++ test_inline.mk
@@ -1,12 +1,31 @@
-test_inline: clean utils.o main.o
-	$(CC) utils.o main.o -o test_inline
-	-objdump -t utils.o | grep get_tu_name
-	-objdump -t main.o  | grep get_tu_name
-	-objdump -t test_inline | grep get_tu_name
-	-objdump -t utils.o | grep convert2
-	-objdump -t main.o  | grep convert2
-	-objdump -t test_inline | grep convert2
-	./test_inline
+srcs:=$(shell find . -maxdepth 1 -name "*.c")
+objs:=$(subst .c,.o,$(srcs))
+bin:=test_inline
+inline_symbols = get_tu_name convert2
+
+reverse = $(if $(wordlist 2,2,$(1)),$(call reverse,$(wordlist 2,$(words $(1)),$(1))) $(firstword $(1)),$(1))
+
+ordered_objs=$(sort $(objs))
+reversed_objs=$(call reverse,$(ordered_objs))
+bin_and_objs = $(ordered_objs) $(bin)
+
+define dump_inline_symbols
+	for inline_symbol in $(inline_symbols); do \
+		for obj in $(bin_and_objs); do \
+			echo "in $$obj:" && objdump -t $$obj | grep $$inline_symbol || (echo "no $$inline_symbol found!") ; \
+		done; \
+	done
+endef
+
+.PHONY: clean ordered_build reversed_build
+
+ordered_build: clean $(ordered_objs) $(bin)
+reversed_build: clean $(reversed_objs) $(bin)
+
+$(bin): $(objs)
+	$(CC) $(objs) -o $(bin)
+	$(call dump_inline_symbols)
+	echo "run test:" && ./test_inline
 
 clean:
 	rm -f *.o
